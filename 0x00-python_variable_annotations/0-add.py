b291f11b7ec55798b4ecf164b7f97d335a871336
OBigVee <adedejivictor98@gmail.com>
2022-04-15 18:03:30 +0100

    feat:
    -Add add.py file
    function add is type annotated

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/p"""ython3
+""" Basic annotations -add """
 
-def add(a:float, b:float)->float:
+
+def add(a: float, b: float) -> float:
+    """return sum of a,b"""
     sum = a + b
     return sum
