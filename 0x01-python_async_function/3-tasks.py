fb7e4a63ab6ed16477f3f9fa53d401378bdcb8e8
OBigVee <adedejivictor98@gmail.com>
2022-04-22 20:03:14 +0100

    fix
    fix shebang line bug

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""" micmic wait-random """
+"""micmic wait-random """
 import asyncio
+
+
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """ new Task is created  asynchronously
-    """
+    """ new Task is created  asynchronously """
     return asyncio.create_task(wait_random(max_delay))
