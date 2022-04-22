dc6e6cb65bb06189d9ccb4ba1c1cb9623a34e187
OBigVee <adedejivictor98@gmail.com>
2022-04-22 20:25:43 +0100

    task4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""alter wait_n function into a new function task_wait_n"""
+import asyncio
+from typing import List
+
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """alter wait_n into task_wait_n"""
+    delay_task_list = []
+    spawn_task = []
+    for spawn in range(n):
+        spawn_task.append(task_wait_random(max_delay))
+    spawn_task = asyncio.as_completed(spawn_task)
+    for spawn in spawn_task:
+        delay_task_list.append(await spawn)
+    return delay_task_list
