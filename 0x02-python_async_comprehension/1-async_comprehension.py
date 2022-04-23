091c8483dabfb999ad17f654a55a65f3a3975a4a
OBigVee <adedejivictor98@gmail.com>
2022-04-23 03:51:50 +0100

    Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,6 +5,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """collect async generated list and return it"""
+    """ collect async generated list """
     result = [i async for i in async_generator()]
     return result
