1ec58392e1c7d6cfd798a71e791186121437328b
OBigVee <adedejivictor98@gmail.com>
2022-05-13 21:30:08 +0100

    parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,11 +10,11 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand(
         [
-            (
-                ({"a": 1}, ("a",), 1),
-                ({"a": {"b": 2}}, ("a",), {"b": 2}),
-                ({"a": {"b": 2}}, ("a", "b"), 2),
-            )
+            
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+            
         ]
     )
     def test_access_nested_map(self, map, path, expected):
@@ -22,10 +22,12 @@ class TestAccessNestedMap(unittest.TestCase):
         output = access_nested_map(map, path)
         self.assertEqual(output, expected)
 
-    @parameterized.expand([
-        ({}, ("a",), "a"),
-         ({"a": 1}, ("a", "b"), "b")
-         ])
+    @parameterized.expand(
+        [
+            ({}, ("a",), "a"),
+            ({"a": 1}, ("a", "b"), "b"),
+        ]
+    )
     def test_access_nested_map_exception(self, map, path, expected):
         """ """
         with self.assertRaises(KeyError) as e:
