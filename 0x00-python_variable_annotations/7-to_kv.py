2ef73501d226f4631219e9a604ef9bb68ef613d1
obigvee <adedejivictor98@gmail.com>
2023-12-09 06:03:38 +0100

    task 7  completed :wrench:

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """type-annotation function"""
-from typing import List, Union, Tuple
+from typing import  Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple"""
     return (k, v ** 2)
