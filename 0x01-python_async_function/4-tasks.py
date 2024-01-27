d2f18cb8b218a5a78c0489d71a037d69a78ce553
obigvee <adedejivictor98@gmail.com>
2024-01-27 17:47:48 +0100

    add 4-tasks.py :heavy_check_mark: - test file 4-main.py added

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,6 +7,13 @@ wait_n = __import__("1-concurrent_coroutines").wait_n
 task_wait_random = __import__("3-tasks").task_wait_random
 
 
-def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ """
-    return task_wait_random(n, max_delay)
+    delay_list = []
+    spawn_list = []
+    for spawn in range(n):
+        spawn_list.append(task_wait_random(max_delay))
+    spawn_list = asyncio.as_completed(spawn_list)
+    for spawn in spawn_list:
+        delay_list.append(await spawn)
+    return delay_list
