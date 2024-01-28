657da57108400b0e6188f9e10989be2401fb9776
obigvee <adedejivictor98@gmail.com>
2024-01-28 20:32:31 +0100

    add file 0-async_comprehension.py and :heavy_check_mark: task 1 completed - test file added 1-main.py

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Async comprehensions list"""
+
+from typing import List
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """collect async generated list"""
+    # collector = []
+    # async for i in async_generator():
+    #     collector.append(i)
+    # return collector
+
+    return [i async for i in async_generator()]
